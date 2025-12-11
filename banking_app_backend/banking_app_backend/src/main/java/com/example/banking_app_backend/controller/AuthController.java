3b6e1b6bcecb43abe1779848d945729cc353d8ba
teki60 <vrnmurthyteki@gmail.com>
2025-12-11 23:34:05 +0530

    Implemented Authentication controller

--- banking_app_backend/banking_app_backend/src/main/java/com/example/banking_app_backend/controller/AuthController.java
+++ banking_app_backend/banking_app_backend/src/main/java/com/example/banking_app_backend/controller/AuthController.java
@@ -4,9 +4,8 @@ import com.example.banking_app_backend.dto.login.LoginRequest;
 import com.example.banking_app_backend.dto.login.LoginResponse;
 import com.example.banking_app_backend.dto.register.RegisterRequest;
 import com.example.banking_app_backend.dto.register.RegisterResponse;
-import com.example.banking_app_backend.service.AuthServiceImpl;
+import com.example.banking_app_backend.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,17 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthServiceImpl authService;
+    private final AuthService authService;
 
     @PostMapping("/register")
     public RegisterResponse registerUser(@RequestBody RegisterRequest registerRequest){
+        authService.register(registerRequest.getUserName(), registerRequest.getPassword());
         RegisterResponse registerResponse = new RegisterResponse();
         return registerResponse;
     }
 
     @PostMapping("/login")
     public LoginResponse loginUser(@RequestBody LoginRequest loginRequest){
+        boolean result = authService.login(loginRequest.getUserName(), loginRequest.getPassword());
         LoginResponse loginResponse = new LoginResponse();
+        if(result){
+            loginResponse.setResult(true);
+            loginResponse.setMessage("User Logged in succesfully");
+        }
+        else{
+            loginResponse.setResult(false);
+            loginResponse.setMessage("Invalid Username or Password");
+        }
         return loginResponse;
     }
 
